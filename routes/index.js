65dde63bbacc51289a638ce034503c6d7199d186
zoraaga1 <fati.sousse12@gmail.com>
2024-08-04 00:31:20 +0100

    task5

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -11,12 +12,11 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
-// New authentication endpoints
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-
-// User information endpoint
 router.get('/users/me', UsersController.getMe);
+
+// New file upload endpoint
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
