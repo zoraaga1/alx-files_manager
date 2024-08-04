d946cbaee9e14f735d6813bf3b4bad126f0d0d22
zoraaga1 <fati.sousse12@gmail.com>
2024-08-04 20:53:15 +0100

    task3

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 // server.js
 
 const express = require('express');
-
-const app = express();
-
 const routes = require('./routes/index');
 
-const port = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware to parse JSON bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// Load routes
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
