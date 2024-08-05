53e9ad71fb7cd6f0ec38d5b59454c250e7dca783
zoraaga1 <fati.sousse12@gmail.com>
2024-08-05 16:53:05 +0100

    task3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,33 @@
 // controllers/UsersController.js
 
-const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class UsersController {
+  // Existing methods (postNew, etc.)
+
   static async getMe(req, res) {
     const token = req.headers['x-token'];
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
+    // Retrieve user ID from Redis
     const userId = await redisClient.get(`auth_${token}`);
     if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
+    // Retrieve user details from MongoDB
     const db = dbClient.client.db(dbClient.dbName);
-    const usersCollection = db.collection('users');
-    const user = await usersCollection.findOne({ _id: new dbClient.client.ObjectId(userId) });
+    const user = await db.collection('users').findOne({ _id: new dbClient.client.ObjectId(userId) });
 
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const { email, _id } = user;
-    return res.status(200).json({ id: _id.toString(), email });
+    // Return user information
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -16,3 +16,5 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 // User information endpoint
 router.get('/users/me', UsersController.getMe);
+
+module.exports = router;
