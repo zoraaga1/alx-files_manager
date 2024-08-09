22f9d7c0442fdd505b0071ccb31c651116acc075
zoraaga1 <fati.sousse12@gmail.com>
2024-08-09 17:28:25 +0100

    task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,11 +3,9 @@ const dbClient = require('../utils/db');
 
 class AppController {
   static getStatus(req, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    res.status(200).json(status);
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
   static async getStats(req, res) {
