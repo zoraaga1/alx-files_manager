51432d82bc14a5bd6dd8bbf6e6751c69bce506bd
zoraaga1 <fati.sousse12@gmail.com>
2024-08-09 22:01:45 +0100

    task 2

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
@@ -10,5 +11,8 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
