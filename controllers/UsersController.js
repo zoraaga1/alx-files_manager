890a0e5de8ea41961d9efb6466e9943c1b7beacd
zoraaga1 <fati.sousse12@gmail.com>
2024-08-09 23:23:30 +0100

    task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 import sha1 from 'sha1';
 import Queue from 'bull/lib/queue';
 import dbClient from '../utils/db';
@@ -6,42 +7,34 @@ const userQueue = new Queue('email sending');
 
 export default class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
 
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
-
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
+    const user = await (await dbClient.usersCollection()).findOne({ email });
 
-    const usersCollection = await dbClient.usersCollection();
-    const existingUser = await usersCollection.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exists' });
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
     }
-
-    const hashedPassword = sha1(password);
-    const insertionInfo = await usersCollection.insertOne({
-      email,
-      password: hashedPassword,
-    });
-
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
     const userId = insertionInfo.insertedId.toString();
 
     userQueue.add({ userId });
-
-    return res.status(201).json({ email, id: userId });
+    res.status(201).json({ email, id: userId });
   }
 
   static async getMe(req, res) {
     const { user } = req;
 
-    return res.status(200).json({
-      email: user.email,
-      id: user._id.toString(),
-    });
+    res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
