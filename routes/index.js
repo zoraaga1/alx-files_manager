148b532e9a889b4a2616eee349c749855070c220
zoraaga1 <fati.sousse12@gmail.com>
2024-08-09 22:18:56 +0100

    task 2

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,6 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
 
 module.exports = router;
